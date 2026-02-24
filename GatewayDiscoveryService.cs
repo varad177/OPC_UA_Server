9ec259fdb0098aaeece572a0c64cc4384b95496c
Varad Dhumale <varaddhumale177@gmail.com>
2026-02-24 17:45:25 +0530

    fix:cert removed

--- GatewayDiscoveryService.cs
+++ GatewayDiscoveryService.cs
@@ -21,6 +21,8 @@ public static class GatewayDiscoveryService
                 var data = udp.Receive(ref ep);
                 var msg = Encoding.UTF8.GetString(data);
 
+            Console.WriteLine("data received: " + msg);
+            Console.WriteLine("from: " + ep.Address);
                 if (msg == "WHO_IS_GATEWAY?")
                 {
                     string ip = GetWifiIPAddress();
